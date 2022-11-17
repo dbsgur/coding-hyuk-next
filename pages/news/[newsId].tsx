b6f08a196ec32f3e9b764bf3765d21e6099a9cb6
dbsgur <kkr431@naver.com>
2022-11-17 23:24:42 +0900

    [feature] next dynamic routing

--- pages/news/[newsId].tsx
+++ pages/news/[newsId].tsx
@@ -3,5 +3,11 @@ import { useRouter } from "next/router";
 
 export default function Detail() {
   const router = useRouter();
+  const newsId = router.query.newsId;
+
+  console.log(`newsId : ${newsId}`)
+  console.log(`router.query : ${JSON.stringify(router.query.newsId)}`)
+  console.log(`${router.query.slug}`)
+
   return <h1>Detail Page</h1>;
 }
--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,9 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Fragment } from 'react'
+import Link from "next/link";
 
 export default function News() {
-  return (<h1>
-    News Page
-  </h1>
+  return (
+  <Fragment>
+    <h1>The News Page</h1>
+    <ul>
+      <li><Link href="/news/hyeok" >Coding Hyeok ... </Link></li>
+      <li>10심히 코딩중</li>
+      </ul>
+  </Fragment>
   )
 }
