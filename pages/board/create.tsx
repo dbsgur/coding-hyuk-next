b18a993e174bef7e381ffdb154a9d2e84d6e0db0
dbsgur <kkr431@naver.com>
2022-11-19 19:35:08 +0900

    [feature] board read and detail page create

--- pages/board/create.tsx
+++ pages/board/create.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Create() {
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("second");
+
+  const onChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+  const onChangeContent = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.target.value);
+  };
+  const onClickSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`title : ${title}`);
+    console.log(`content : ${content}`);
+  };
   return (
-    <form>
-      <input type="text" placeholder="제목을 입력해주세요." />
-      <textarea />
+    <form onSubmit={onClickSubmit}>
+      <input
+        type="text"
+        placeholder="제목을 입력하세요."
+        onChange={onChangeTitle}
+        value={title}
+      />
+      <textarea
+        placeholder="내용을 입력하세요."
+        value={content}
+        onChange={onChangeContent}
+      />
       <input type="submit" value="부웅" />
     </form>
   );
