84ff786dcb2a6dc46c8210b16f44a276e9a1dbce
dbsgur <kkr431@naver.com>
2022-11-20 15:43:44 +0900

    [fix] routerId Error fix + boardcreate onChange Function refactor

--- pages/board/index.tsx
+++ pages/board/index.tsx
@@ -1,44 +1,42 @@
 import RouterBtn from "../../component/routerButton";
-import { Fragment } from "react";
 import Link from "next/link";
-
-type Board = { id: number; title: string; content: string };
-
-const boards: Board[] = [
-  { id: 1, title: "CODING", content: "혁이는 코딩중" },
-  { id: 2, title: "PLAYING", content: "현석이도 노는중" },
-  { id: 3, title: "CODING", content: "건희형도 코딩중" },
-  { id: 4, title: "CODING", content: "동규도 코딩중" },
-  { id: 5, title: "CODING", content: "원영이도 코딩중" },
-  { id: 6, title: "PLAYING", content: "딸기는 노는중" },
-  { id: 7, title: "FUCK", content: "YOU~~~~~~" },
-];
+import { boards, BoardStruct } from "../../data/boardData";
+import { useState, useEffect } from "react";
 
 function Board() {
-  return (
-    <Fragment>
-      <table border={2}>
-        <th>ID</th>
-        <th>TITLE</th>
-        <th>Content</th>
-        {!boards ? (
-          <h1>데이터가 없어용</h1>
-        ) : (
-          boards.map((board: Board) => (
-            <tr key={board.id}>
-              {/* router.push로 변경 */}
-              <Link href={`/board/${board.id}_${board.title}_${board.content}`}>
-                <td>{board.id}</td>
-                <td>{board.title}</td>
-                <td>{board.content}</td>
-              </Link>
-            </tr>
-          ))
-        )}
-      </table>
+  // Error: Hydration failed because the initial UI does not match what was rendered on the server.
 
+  return (
+    <div>
+      <table border={2}>
+        <thead>
+          <th>ID</th>
+          <th>TITLE</th>
+          <th>Content</th>
+        </thead>
+        <tbody>
+          <div>
+            {!boards ? (
+              <td>데이터가 없어용</td>
+            ) : (
+              boards.map((board: BoardStruct) => (
+                <tr key={board.id}>
+                  {/* router.push로 변경 */}
+                  <Link
+                    href={`/board/${board.id}_${board.title}_${board.content}`}
+                  >
+                    <td>{board.id}</td>
+                    <td>{board.title}</td>
+                    <td>{board.content}</td>
+                  </Link>
+                </tr>
+              ))
+            )}
+          </div>
+        </tbody>
+      </table>
       <RouterBtn title="CREATE" url="/board/create" />
-    </Fragment>
+    </div>
   );
 }
 
