36da6ae6ad3d752263604aad90d749e1bb9d602c
dbsgur <kkr431@naver.com>
2022-11-26 20:13:48 +0900

    [setting] mongoDB connect

--- pages/enviorTest.tsx
+++ pages/enviorTest.tsx
@@ -1,3 +1,6 @@
+import dbConnect from "lib/db/dbConnect";
+import Item from "lib/schema/Item";
+
 export default function enviorTest() {
   console.log(
     "----------------CLIENT ENVIRONMENT VARIABLES TEST----------------"
@@ -15,12 +18,22 @@ export default function enviorTest() {
   );
 }
 
-export function getServerSideProps() {
+export async function getServerSideProps() {
+  await dbConnect();
+  const result = await Item.find({});
+  const items = result.map((doc) => {
+    const item = doc.toObject();
+    item._id = item._id.toString();
+    return item;
+  });
+
   console.log(
     "----------------SERVER ENVIRONMENT VARIABLES TEST----------------"
   );
   console.log("process.env.NODE_ENV : ", process.env.NODE_ENV);
   console.log("process.env.TEST : ", process.env.TEST);
   console.log("process.env.NEXT_PUBLIC_TEST : ", process.env.NEXT_PUBLIC_TEST);
-  return { props: {} };
+
+  return { props: { items: items } };
+  // return { props: {} };
 }
