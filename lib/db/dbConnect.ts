c547e76327e05ea7a734c5c36db49dca52479cd6
dbsgur <kkr431@naver.com>
2022-11-27 23:35:19 +0900

    [chore]

---
+++ lib/db/dbConnect.ts
@@ -0,0 +1,51 @@
+import { Schema, model, connect } from "mongoose";
+
+// 1. Create an interface representing a document in MongoDB.
+interface IUser {
+  name: string;
+  email: string;
+  avatar?: string;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  avatar: String,
+});
+
+// 3. Create a Model.
+const User = model<IUser>("User", userSchema);
+
+dbConnect().catch((err) => console.log(err));
+
+async function dbConnect() {
+  // 4. Connect to MongoDB
+  await connect(process.env.MONGODB_URI!);
+  // await connect(process.env.MONGODB_URI || '' );
+
+  const user = new User({
+    name: "Bill",
+    email: "bill@initech.com",
+    avatar: "https://i.imgur.com/dM7Thhn.png",
+  });
+  await user.save();
+
+  console.log(user.email); // 'bill@initech.com'
+}
+
+export default dbConnect;
+// import mongoose from "mongoose";
+
+// const connection = {};
+
+// async function dbConnect() {
+//   if (connection.isConnected) {
+//     return;
+//   }
+
+//   const db = await mongoose.connect(process.env.MONGODB_URI);
+//   connection.isConnected = db.connections[0].readyState;
+// }
+
+// export default dbConnect;
