56ab1ab6948906890540585c129fda22a08e9e29
dbsgur <kkr431@naver.com>
2022-12-01 22:20:19 +0900

    google oauth connect success

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -2,15 +2,31 @@ import React from "react";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import RouterBtn from "./RouterBtn";
 import { navs } from "data/navData";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 function Navbar() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <Breadcrumbs maxItems={5} aria-label="breadcrumb">
-      {navs.map((nav) => (
-        <RouterBtn title={nav.title} url={nav.url} key={nav.url} />
-      ))}
-    </Breadcrumbs>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   );
+  // return (
+  // <Breadcrumbs maxItems={5} aria-label="breadcrumb">
+  //   {navs.map((nav) => (
+  //     <RouterBtn title={nav.title} url={nav.url} key={nav.url} />
+  //   ))}
+  // </Breadcrumbs>
+  // );
 }
 
 export default Navbar;
